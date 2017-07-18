5b7f0495ac230cf9d4b8b135d599503cc27a289c
YifengZhong <yf_zhong@hotmail.com>
2017-07-18 14:05:50 -0500

    runnable status for making order object

--- Shipment/src/sql/populate.sql
+++ Shipment/src/sql/populate.sql
@@ -9,5 +9,8 @@ INSERT INTO authority (username, authority) VALUES ('admin', 'ROLE_USER');
 INSERT INTO  `SINGLEORDER` (orderNumber, quantity,status) VALUES ('O123-4566',12,'Delivering');
 INSERT INTO `SINGLEORDER` (orderNumber, quantity, status) VALUES ('O123-5566',123,'Preparing');
 
+INSERT INTO 'USERINFO' (street, city, state, zipCode, userName) VALUES ('N ST 4 1000', 'FairField', 'IOWA','51227','John');
+INSERT INTO 'USERINFO' (street, city, state, zipCode, userName) VALUES ('N ST 4 1001', 'FairField', 'IOWA','51227','Jim');
+
 
 					
