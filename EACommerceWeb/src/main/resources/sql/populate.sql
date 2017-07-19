838ab89af3e2a8107c1e9e5947d0c927aa6ad3f1
YifengZhong <yf_zhong@hotmail.com>
2017-07-19 01:27:36 -0500

    solve the login problem of spring security

--- EACommerceWeb/src/main/resources/sql/populate.sql
+++ EACommerceWeb/src/main/resources/sql/populate.sql
@@ -4,6 +4,11 @@ INSERT INTO product VALUES(3,'brand new','Shorts',25.99,'p321');
 
  INSERT INTO usercredential VALUES ('Paul','','$2a$10$cbn04TjztMMe9iQiIk3tT.sv3HBDg8FGnlO8UIlaKtP5PtiNVqPka',NULL);
  INSERT INTO usercredential VALUES ('Sean','','$2a$10$q2mJubO41mryHffrWrEi6e63BDdCgcadq36pXUXnBunZUxDq96Atu',NULL);
+ 
+INSERT INTO authority (username, authority) VALUES ('guest', 'ROLE_ADMIN');
+INSERT INTO authority (username, authority) VALUES ('Paul', 'ROLE_ADMIN');
+INSERT INTO authority (username, authority) VALUES ('Sean', 'ROLE_ADMIN');
+ 
 
 INSERT INTO user VALUES (1,30,'Paul','Bearer', 7,"",'Paul'); 
 INSERT INTO user VALUES (2, 45, 'Sean','Smith',1,"",'Sean');
